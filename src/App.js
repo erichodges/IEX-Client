41d1e2bc4fc047e19247b91e771de7409ca97d0c
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-10 21:11:51 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      reply: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://api.iextrading.com/1.0//stock/aapl/chart/1m")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          loading: false,
+          reply: data
+        });
+      });
+  }
+
   render() {
+    const text = this.state.loading ? "loading..." : this.state.reply[0].change;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>{text}</p>
       </div>
     );
   }
