0d1b25a2fb7f10b0361a488f326c36b108e0198c
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-12 20:21:17 -0800

    added basic Quote functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
 import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       loading: false,
-      reply: {}
+      tickers: [],
+      data: []
     };
   }
 
-  componentDidMount() {
-    this.setState({ loading: true });
-    fetch("https://api.iextrading.com/1.0//stock/aapl/chart/1m")
-      .then(response => response.json())
-      .then(data => {
+  getQuote(ticker) {
+    // this.setState({ loading: true });
+    let query = this.newTicker.value;
+    axios
+      .get(`https://api.iextrading.com/1.0/stock/${query}/quote`)
+      .then(res => {
+        const data = res.data;
+        this.setState({ data, loading: false });
         console.log(data);
-        this.setState({
-          loading: false,
-          reply: data[0]
-        });
       });
+    this.addForm.reset();
+  }
+
+  addTicker(e) {
+    e.preventDefault();
+    // const { tickers } = this.state.tickers;
+    const newTicker = this.newTicker.value;
+
+    this.setState({
+      tickers: [...this.state.tickers, newTicker]
+    });
   }
 
   render() {
-    const text = this.state.loading ? "loading..." : this.state.reply.change;
-    return (
-      <div>
-        <p>{text}</p>
-      </div>
-    );
+    if (!this.state.loading) {
+      return (
+        <div>
+          <form
+            className="ticker-form"
+            onSubmit={e => {
+              this.addTicker(e);
+              this.getQuote(this.newTicker);
+            }}
+            ref={input => (this.addForm = input)}
+          >
+            <div className="form-group">
+              <label className="inputLabel" htmlFor="newTickerInput">
+                Quote
+              </label>
+              &nbsp;
+              <input
+                ref={input => (this.newTicker = input)}
+                type="text"
+                placeholder="Add a Ticker"
+                className="tickerInput"
+              />
+              &nbsp;
+              <button type="submit" className="tickerSubmit">
+                Add
+              </button>
+            </div>
+          </form>
+
+          <p>
+            {this.state.data.symbol} {this.state.data.latestPrice}
+          </p>
+        </div>
+      );
+    } else {
+      return "Loading...";
+    }
   }
 }
 
