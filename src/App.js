d2c3f27e8371c036afc370571791e8fc1b6e07bc
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-23 14:44:38 -0800

    initial commit for chart branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-// import axios from "axios";
 import Quote from "./components/Quote";
+import Chart from "./components/Chart";
+import ChartComponent from "./components/index";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ChartComponent />
+        <Chart />
         <Quote />
         <Quote />
       </div>
---
+++ src/components/Utils.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+// import React, { Component } from "react";
+// import { tsvParse, csvParse } from "d3-dsv";
+// import { timeParse } from "d3-time-format";
+
+export function getData() {
+  let endpoint = `https://api.iextrading.com/1.0/stock/aapl/chart/1y`;
+  axios.get(endpoint).then(res => {
+    const d = JSON.stringify(res.data);
+    console.log(d);
+    return d;
+  });
+}
+
+// function parseData(parse) {
+//   return function(d) {
+//     d.date = parse(d.date);
+//     d.open = +d.open;
+//     d.high = +d.high;
+//     d.low = +d.low;
+//     d.close = +d.close;
+//     d.volume = +d.volume;
+// 		console.log(d);
+//     return d;
+//   };
+// }
+
+// const parseDate = timeParse("%Y-%m-%d");
+
+// export function getData() {
+//   const promiseMSFT = fetch(
+//     "https://cdn.rawgit.com/rrag/react-stockcharts/master/docs/data/MSFT.tsv"
+//   )
+//     .then(response => response.text())
+//     .then(data => tsvParse(data, parseData(parseDate)))
+
+//   return promiseMSFT;
+// }
