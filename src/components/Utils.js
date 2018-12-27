cb1f761c53c16f9f9b0c400eeb8695553dd8beab
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-27 14:49:21 -0800

    reset Utils

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,26 +1,11 @@
-import React from "react";
-import PropTypes from "prop-types";
 import axios from "axios";
 import { timeParse } from "d3-time-format";
 // import React, { Component } from "react";
 // import { tsvParse, csvParse } from "d3-dsv";
 
-class Utils extends React.Component {
-  render() {
-    return <p>{this.props.text}</p>;
-  }
-}
-Utils.propTypes = {
-  text: PropTypes.string
-};
-Utils.defaultProps = {
-  text: null
-};
-
 const parseDate = timeParse("%Y-%m-%d");
 
 export async function getData() {
-  // e.preventDefault();
   const endpoint = "https://api.iextrading.com/1.0/stock/spy/chart/3m";
   return await axios.get(endpoint).then(res => {
     return res.data.map(item => {
