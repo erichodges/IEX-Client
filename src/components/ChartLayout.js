c5f5d2be9f468fe8c6d97c26e478d7e744f8db47
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-02-24 13:34:49 -0800

    in Chartlayout iexRealtimePrice, // This causes no data to load off hours. Switched back to close price instead

--- src/components/ChartLayout.js
+++ src/components/ChartLayout.js
@@ -69,11 +69,11 @@ class ChartLayout extends Component {
         open: values[2].open,
         high: values[2].high,
         low: values[2].low,
-        close: values[2].iexRealtimePrice, // This causes no data to load off hours
+        close: values[2].close, // iexRealtimePrice causes no data to load off hours
         volume: values[2].latestVolume,
         oldTicker: "SPY"
       });
-      console.log(this.state.data.close);
+      // console.log(this.state.data.close);
     });
   }
   handleChartSubmit(e, ticker, time) {
