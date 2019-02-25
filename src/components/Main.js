3e6ffd389912dd94468f8ca0671171e16b57b5be
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-02-25 13:53:09 -0800

    initial commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ChartLayout from "./ChartLayout";
 import Quote from "./Quote";
+import shortid from "shortid";
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tickerFromQuote: ""
+      tickerFromQuote: "",
+      items: []
     };
 
     this.onSendQuoteTicker = this.onSendQuoteTicker.bind(this);
@@ -16,10 +18,40 @@ class Main extends React.Component {
       tickerFromQuote: ticker
     });
   }
+  
+  onRemoveChild = item => {
+    this.setState(prev => ({
+      items: prev.items.filter(prevItem => prevItem.id !== item.id)
+    }));
+  };
+
+  onAddChild = item => {
+    this.setState(prev => ({
+      items: [...prev.items, { id: shortid.generate()}]    
+    }));
+  }
+
+  const ParentComponent = props => {
+    <div>
+      <button onClick={props.addChild}>Add Quote List</button>
+      <div id="children-pane">{props.children}</div>
+    </div>
+  };
+
   render() {
     return (
       <div>
         <ChartLayout symbol={this.state.tickerFromQuote} />
+        <ParentComponent addChild={this.onAddChild}>
+          {this.state.items.map(item => {
+            <Quote 
+              key={item.id}
+              item={item}
+              remove={this.onRemoveChild}
+              setTickerForChart={this.onSendQuoteTicker}
+            />
+          })}
+        </ParentComponent>
         <Quote setTickerForChart={this.onSendQuoteTicker} />
       </div>
     );
