8f8d969d27548d63674124a6e10449d0bcc2f5b9
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-03-01 14:43:13 -0800

    added some temp styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
+const ulStyle = {
+  listStyle: "none",
+  display: "flex",
+  alignItems: "center"
+};
+
+const logoStyle = {
+  marginRight: "auto"
+};
+
+const userItemsStyle = {
+  marginRight: "2rem"
+};
 class Header extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>
-            <h3>&nbsp;&nbsp;&nbsp; IEX Data App</h3>
+        <ul style={ulStyle}>
+          <li style={logoStyle}>
+            <h3>IEX Data App</h3>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/login" style={userItemsStyle}>
+              Login
+            </Link>
           </li>
           <li>
-            <Link to="/register">Register</Link>
+            <Link to="/register" style={userItemsStyle}>
+              Register
+            </Link>
           </li>
         </ul>
       </div>
