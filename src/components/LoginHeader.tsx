2ac50edf1750f04e5dace14fc7ad850623428fc8
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-03-17 15:55:55 -0700

    working on userName showing after login

--- src/components/LoginHeader.tsx
+++ src/components/LoginHeader.tsx
@@ -3,7 +3,6 @@ import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 import { MeQuery } from "../schemaTypes";
 import { Link } from "react-router-dom";
-// import { Link } from "react-router-dom";
 
 const meQuery = gql`
   query MeQuery {
@@ -27,23 +26,40 @@ class LoginHeader extends PureComponent {
           if (loading) {
             return null;
           }
-          if (!data) {
-            return (
-              <div>
-                <li>
-                  <Link to="/login" style={userItemsStyle}>
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/register" style={userItemsStyle}>
-                    Register
-                  </Link>
-                </li>
-              </div>
-            );
+          if (data) {
+            if (data.me) {
+              return <div style={userItemsStyle}>{data.me!.userName}</div>;
+            } else {
+              return (
+                <div>
+                  <li>
+                    <Link to="/login" style={userItemsStyle}>
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/register" style={userItemsStyle}>
+                      Register
+                    </Link>
+                  </li>
+                </div>
+              );
+            }
           }
-          return <div style={userItemsStyle}>{data.me!.userName}</div>;
+          return (
+            <div>
+              <li>
+                <Link to="/login" style={userItemsStyle}>
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link to="/register" style={userItemsStyle}>
+                  Register
+                </Link>
+              </li>
+            </div>
+          );
         }}
       </Query>
     );
