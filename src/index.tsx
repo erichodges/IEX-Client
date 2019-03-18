e1dfe104f26cf4c2fe904d98ac7afcc0d37c4323
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-03-18 13:42:49 -0700

    added InMemoryCache from apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
+import ApolloClient, { InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import "./index.css";
 import Routes from "./Routes";
 // import registerServiceWorker from "./registerServiceWorker";
 // import App from "./App";
 
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  credentials: "include"
+  credentials: "include",
+  cache
 });
 
 ReactDOM.render(
