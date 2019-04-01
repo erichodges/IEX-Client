bfb722162a0ae489a6eeb0cc030d4df39fdf60dc
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-04-01 15:03:39 -0700

    cleaned up ChartTicker imports

--- src/components/ChartTicker.js
+++ src/components/ChartTicker.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import ChartComponent from "./ChartComponent";
 
 class ChartTicker extends Component {
   constructor() {
