bdeb5c91257460424f7b267e101dcefff5d7f25c
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-06-29 14:52:58 -0700

    Changed SaveQuoteList to js from tsx

--- src/modules/user/SaveQuoteList.js
+++ src/modules/user/SaveQuoteList.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 import { Mutation } from "react-apollo";
-import {
-  MeQuery,
-  addQuoteList_addQuoteList,
-  addQuoteListVariables
-} from "../../schemaTypes";
 
 const meQuery = gql`
   query MeQuery {
@@ -34,82 +29,66 @@ const ADD_QUOTE_LIST = gql`
 `;
 
 class SaveQuoteList extends Component {
-  // @ts-ignore
   constructor(props) {
     super(props);
-    // @ts-ignore
+
     this.quoteListName = React.createRef();
 
     this.state = {
       quoteListName: "Quote List"
     };
-    // @ts-ignore
+
     this.onAddQuoteListName = this.onAddQuoteListName.bind(this);
-    // @ts-ignore
     this.onKeyPressed = this.onKeyPressed.bind(this);
-    // @ts-ignore
     this.onAddQuoteListId = this.onAddQuoteListId.bind(this);
   }
-  // @ts-ignore
+
   onAddQuoteListName(e) {
-    // @ts-ignore
     this.props.addQuoteListName(
-      // @ts-ignore
       this.quoteListName.current.value,
-      // @ts-ignore
       this.props.item.id
     );
-    // @ts-ignore
-    this.props.quoteListDisplayName(
-      // @ts-ignore
-      this.quoteListName.current.value
-    );
+
+    this.props.quoteListDisplayName(this.quoteListName.current.value);
     this.setState({
-      // @ts-ignore
       quoteListName: this.quoteListName.current.value
     });
-    // @ts-ignore
     // console.log(this.state.quoteListName);
-    // @ts-ignore
     this.quoteListName.current.value = "";
   }
-  // @ts-ignore
+
   onKeyPressed(e) {
     if (e.key === "Enter") {
       this.onAddQuoteListName(e);
     }
   }
-  // @ts-ignore
+
   onAddQuoteListId(responseWithQuoteList, itemId, name) {
     const quoteListId = responseWithQuoteList.data.addQuoteList.id;
-    // @ts-ignore
+
     this.props.addQuoteListId(quoteListId, itemId, name);
   }
 
   render() {
-    // @ts-ignore
     // const { quoteListName } = this.state;
-    // @ts-ignore
     const quoteListArray = this.props.quoteListArray;
-    // @ts-ignore
     const quoteListId = this.props.item.id;
     // console.log(quoteListArray);
     return (
-      <Query<MeQuery> query={meQuery}>
+      <Query query={meQuery}>
         {({ data, loading }) => {
           if (loading) {
             return <div>Loading...</div>;
           }
 
-          if (!data! || !data!.me!) {
+          if (!data || !data.me) {
             return null;
           }
-          if (data!.me!.userName) {
+          if (data.me.userName) {
             return (
               <div>
                 &nbsp;&nbsp;&nbsp;
                 <input
-                  // @ts-ignore
                   ref={this.quoteListName}
                   onKeyPress={this.onKeyPressed}
                   type="text"
@@ -125,15 +104,10 @@ class SaveQuoteList extends Component {
                   Add
                 </button>
                 &nbsp;
-                <Mutation<addQuoteList_addQuoteList, addQuoteListVariables>
-                  mutation={ADD_QUOTE_LIST}
-                >
+                <Mutation mutation={ADD_QUOTE_LIST}>
                   {mutate => (
-                    // @ts-ignore
                     <button
-                      // @ts-ignore
                       onClick={e => {
-                        // @ts-ignore
                         quoteListArray.map(async item => {
                           const tickers = item.tickers;
                           const name = item.name;
@@ -147,7 +121,6 @@ class SaveQuoteList extends Component {
                                 }
                               ]
                             });
-                            // @ts-ignore
                             this.onAddQuoteListId(response, item.id, name);
                             console.log("From: SaveQuoteList", response);
                           }
