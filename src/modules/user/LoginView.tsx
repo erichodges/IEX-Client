2da72cd556335befb9affda52f78cc408e8baa5a
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-07-16 16:08:27 -0700

    Basic login styles are set up

--- src/modules/user/LoginView.tsx
+++ src/modules/user/LoginView.tsx
@@ -1,9 +1,16 @@
 import React, { PureComponent } from "react";
+import { RouteComponentProps } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Mutation } from "react-apollo";
 import { gql } from "apollo-boost";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+
+import styles from "../../components/styles/LoginView.module.css";
+import DarkTextField from "../../components/styleComponents/DarkTextField";
 import { LoginMutationVariables, LoginMutation } from "../../schemaTypes";
-import { RouteComponentProps } from "react-router-dom";
 
 const loginMutation = gql`
   mutation LoginMutation($email: String!, $password: String!) {
@@ -59,43 +66,70 @@ class LoginView extends PureComponent<RouteComponentProps<{}>> {
       >
         {(mutate, { client }) => (
           <div>
-            <input
-              type="text"
-              name="email"
-              placeholder="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            <button
-              onClick={async () => {
-                const response = await mutate({
-                  variables: this.state
-                });
-                console.log(response);
-                this.props.history.push("/");
-              }}
+            <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+              style={{ minHeight: "100vh" }}
             >
-              Login
-            </button>
-            <div>
-              <br />
-              <br />
-              &nbsp;&nbsp;&nbsp;<Link to="/">Main Page</Link>
-              <br />
-              <br />
-              &nbsp;&nbsp;&nbsp;If you don't have a login, please &nbsp;
-              &nbsp;&nbsp;&nbsp;
-              <Link to="/register" style={userItemsStyle}>
-                Register
-              </Link>
-            </div>
+              <Grid item>
+                <Card className={styles.card}>
+                  <CardContent>
+                    <div className={styles.contentWrapper}>
+                      <DarkTextField
+                        variant="outlined"
+                        type="text"
+                        margin="dense"
+                        name="email"
+                        placeholder="email"
+                        required={true}
+                        value={email}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className={styles.contentWrapper}>
+                      <DarkTextField
+                        margin="dense"
+                        variant="outlined"
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        required={true}
+                        value={password}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className={styles.contentWrapper}>
+                      <Button
+                        onClick={async () => {
+                          const response = await mutate({
+                            variables: this.state
+                          });
+                          console.log(response);
+                          this.props.history.push("/");
+                        }}
+                      >
+                        Login
+                      </Button>
+                    </div>
+                    <div>
+                      <br />
+                      <br />
+                      &nbsp;&nbsp;&nbsp;<Link to="/">Main Page</Link>
+                      <br />
+                      <br />
+                      &nbsp;&nbsp;&nbsp;If you don't have a login, please &nbsp;
+                      &nbsp;&nbsp;&nbsp;
+                      <Link to="/register" style={userItemsStyle}>
+                        Register
+                      </Link>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
           </div>
         )}
       </Mutation>
