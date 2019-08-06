d6af6e7c459144768fb49cc0a04e163aa690e346
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-08-06 15:24:30 -0700

    Working on ChartLayout errors

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -6,15 +6,32 @@ const API_Token = process.env.REACT_APP_IEX_API;
 //
 // https://api.iextrading.com/1.0/stock/${ticker}/chart/${time}
 //
+
 export async function getData(ticker, time) {
     const endpoint = `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${API_Token}`;
-    return await axios.get(endpoint).then(res => {
-      return res.data.map(item => {
-        item.date = parseDate(item.date);
-        return item;
-      });
-    }).catch(e => console.log("Utils: getData" + e.message)); 
+    try {
+        const res = await axios.get(endpoint)
+        return res.data.map(item => {
+            item.date = parseDate(item.date);
+            console.log("Date", item.date)
+            return item;
+        });
+    } catch(e) {
+        console.log("Utils: getData" + e.message);
+          return []
+    }
 }
+ 
+// export async function getData(ticker, time) {
+//     const endpoint = `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${API_Token}`;
+//     return await axios.get(endpoint).then(res => {
+//       return res.data.map(item => {
+//         item.date = parseDate(item.date);
+//         return item;
+//       });
+//     }).catch(e => console.log("Utils: getData" + e.message)); 
+// }
+
 // Thu Oct 25 2018 00:00:00
 // "ddd MMM D  YYYY hh:mm:ss"
 // &displayPercent=true
@@ -40,3 +57,33 @@ export async function getKeyStats(ticker) {
     return res.data;
   }).catch(e => console.log("Utils: getKeyStats" + e.message)); 
 }
+
+
+
+// export async function getData(ticker, time) {
+//     const endpoint = `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${API_Token}`;
+//     try {
+//         const res = await axios.get(endpoint)
+//         return res.data.map(item => {
+//             item.date = parseDate(item.date);
+//             return item;
+//         });
+//     } catch(e) {
+//         console.log("Utils: getData" + e.message);
+//         return []
+//     } 
+// }
+
+
+// export async function getData(ticker, time) {
+//     const endpoint = `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${API_Token}`;
+//     return axios.get(endpoint).then(res => {
+//       return res.data.map(item => {
+//         item.date = parseDate(item.date);
+//         return item;
+//       });
+//     }).catch(e => {
+//         console.log("Utils: getData" + e.message);
+//         return [];
+//     }); 
+// }
